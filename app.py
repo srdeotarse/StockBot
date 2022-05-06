55a3500aaeeee96d4c56841d2b4881d2864a79c2
srdeotarse <srdeotarse@gmail.com>
2022-05-06 22:47:21 +0530

    app: Add selection of channel patterns.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import necessary libraries
+import math
 import numpy as np
 import pandas as pd
 import pandas_datareader as data
@@ -289,10 +290,73 @@ class trendlineSeeker:
                 self.currentTrendlineBreakingPoint = len(self.data) + 1
         if viz: self.vizTrend()
     
+class channelSeeker(trendlineSeeker):
+    def __init__(self, symbols, data):
+        super().__init__(symbols, data, flag='all')
+        self.channelValidationWindow = 48
+        self.slopeDiffThreshold = 8*math.pi/180
+        self.channelBreakingThreshold = 0.1
+        self.channels = {}
+    def runChannelSeeker(self, viz=False):
+        self.runTrendlineSeeker(viz=False)
+        trendlines = self.refinedTrendlines
+        for symbol in self.symbols:
+            self.channels[symbol] = {}
+            upperTrends = trendlines[symbol]['upper']
+            lowerTrends = trendlines[symbol]['lower']
+            for ku, vu in upperTrends.items():
+                uSlope, uIntercept, uStart, uEnd = vu['slope'], vu['intercept'], vu['starting row'], vu['ending row']
+                for kl, vl in lowerTrends.items():
+                    lSlope, lIntercept, lStart, lEnd = vl['slope'], vl['intercept'], vl['starting row'], vl['ending row']
+                    if (np.abs(uSlope-lSlope) < self.slopeDiffThreshold) and (uIntercept-lIntercept > 0) and range(max(uStart, lStart), min(uEnd, lEnd)+1):
+                       start = np.min((uStart, lStart))
+                       end = np.max((uEnd, lEnd))
+                       upperViolation = self.channelBreakingPoints(symbol, uSlope, uIntercept, start, end, self.channelBreakingThreshold, flag='upper')
+                       lowerViolation = self.channelBreakingPoints(symbol, lSlope, lIntercept, start, end, self.channelBreakingThreshold, flag='lower')
+                       if upperViolation.any() or lowerViolation.any():
+                           continue
+                       else:
+                           channelId = f'{start}'
+                           self.channels[symbol][channelId] = {}
+                           self.channels[symbol][channelId]['start'] = start
+                           self.channels[symbol][channelId]['end'] = end
+                           self.channels[symbol][channelId]['slopeUpper'] = uSlope
+                           self.channels[symbol][channelId]['slopeLower'] = lSlope
+                           self.channels[symbol][channelId]['interceptUpper'] = uIntercept
+                           self.channels[symbol][channelId]['interceptLower'] = lIntercept
+                    else:
+                        continue
+        if viz == True:
+            # print(self.channels)
+            self.vizChannel()
+    def channelBreakingPoints(self, symbol, slope, intercept, start, end, tolerance, flag):
+        if flag == 'upper':
+            violationCondition = self.data.loc[:, (symbol,'close')].iloc[start:end] > (1 + tolerance)*(slope*self.data['row number'].iloc[start:end]+1.0*intercept)
+        if flag == 'lower':
+            violationCondition = self.data.loc[:, (symbol,'close')].iloc[start:end] < (1 - tolerance)*(slope*self.data['row number'].iloc[start:end]+1.0*intercept)
+        # print(self.data.loc[:, (symbol, 'close')].iloc[start:end]-(1 - tolerance)*(slope*self.data['row number'].iloc[start:end]+1.0*intercept))
+        return self.data.iloc[start:end].loc[violationCondition, 'row number']
+    def vizChannel(self):
+        fig, axs = plt.subplots(len(self.symbols))
+        pltCount = 0
+        for symbol, values in self.channels.items():
+            tempAxis = axs[pltCount] if len(self.symbols) > 1 else axs
+            tempAxis.plot(self.data.index,self.data.loc[:, (symbol, "close")], label=f'{symbol} price action') 
+            for channelId, channelProperties in values.items():
+                  tempAxis.plot(
+self.data.index.values[channelProperties['start']:channelProperties['end']], channelProperties['slopeUpper']*self.data['row number'][channelProperties['start']:channelProperties['end']] + channelProperties['interceptUpper'])
+                  tempAxis.plot(
+self.data.index.values[channelProperties['start']:channelProperties['end']],channelProperties['slopeLower']*self.data['row number'][channelProperties['start']:channelProperties['end']] + channelProperties['interceptLower'])
+            pltCount += 1
+        chart = plt.show()
+        st.pyplot(chart)  
 
 if __name__ == '__main__':
     data = get_data(TICKER, START_DATE, END_DATE)
+    st.subheader('Stock Chart Trendlines')
     ts = trendlineSeeker([TICKER], data, flag='all')
     ts.runTrendlineSeeker(viz = True)
-
+    st.subheader('Detected Channel Patterns')
+    cs = channelSeeker([TICKER], data)
+    cs.runChannelSeeker(viz=True)
 st.set_option('deprecation.showPyplotGlobalUse', False)
